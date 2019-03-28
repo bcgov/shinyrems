7eaf79603671ddb7d2dcb18a23aba5d83b351f81
Seb Dalgarno <seb@poissonconsulting.ca>
2019-03-27 18:53:15 -0700

    check for db, get sql date range

---
+++ R/utils.R
@@ -0,0 +1,30 @@
+selectInputX <- function(...) {
+  selectizeInput(..., multiple = TRUE, options = list(
+    'plugins' = list('remove_button'),
+    'create' = TRUE,
+    'persist' = FALSE))
+}
+
+check_historic_db <- function(){
+  db_path <- rems:::write_db_path()
+  if (!file.exists(db_path)){
+    stop("You have not downloaded the historic database. Please follow instructions at 'https://github.com/poissonconsulting/shinyrems'")
+  }
+}
+
+max_db_date <- function(){
+  db_path <- rems:::write_db_path()
+  conn <- DBI::dbConnect(RSQLite::SQLite(), dbname = db_path)
+  DBI::dbGetQuery(conn, "SELECT MAX(COLLECTION_START) FROM historic") %>%
+    rems::ems_posix_numeric() %>%
+    as.Date()
+}
+
+min_db_date <- function(){
+  db_path <- rems:::write_db_path()
+  conn <- DBI::dbConnect(RSQLite::SQLite(), dbname = db_path)
+  DBI::dbGetQuery(conn, "SELECT MIN(COLLECTION_START) FROM historic") %>%
+    rems::ems_posix_numeric() %>%
+    as.Date()
+}
+
