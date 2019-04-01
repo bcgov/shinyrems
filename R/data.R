0f825c1f221c31d7eb202d583fa3788a7a9d3e7d
Seb Dalgarno <seb@poissonconsulting.ca>
2019-04-01 15:29:50 -0700

    remove parameters and sites, simplify data-raw

--- R/data.R
+++ R/data.R
@@ -1,59 +1,6 @@
 #' EMS demo data.
 #'
-#' EMS data including a subset of parameters and sites.
+#' EMS data for three paramters ("Temperature", "pH", "Turbidity") and 1000 sites.
 #'
-#' @format A data frame with 907694 rows and 22 variables:
-#' \describe{
-#'   \item{price}{price, in US dollars}
-#'   \item{carat}{weight of the diamond, in carats}
-#'   ...
-#' }
+#' @format A data frame with 250241 rows and 22 variables:
 "ems_demo_data"
-
-#' EMS parameters.
-#'
-#' EMS lookup table for which sites have data for each parameter.
-#'
-#' @format A data frame with 907694 rows and 22 variables:
-#' \describe{
-#'   \item{price}{price, in US dollars}
-#'   \item{carat}{weight of the diamond, in carats}
-#'   ...
-#' }
-"ems_demo_parameters"
-
-#' EMS sites.
-#'
-#' EMS lookup table for sites and their location.
-#'
-#' @format A data frame with 907694 rows and 22 variables:
-#' \describe{
-#'   \item{price}{price, in US dollars}
-#'   \item{carat}{weight of the diamond, in carats}
-#'   ...
-#' }
-"ems_demo_sites"
-
-#' EMS parameters.
-#'
-#' EMS lookup table for which sites have data for each parameter.
-#'
-#' @format A data frame with 907694 rows and 22 variables:
-#' \describe{
-#'   \item{price}{price, in US dollars}
-#'   \item{carat}{weight of the diamond, in carats}
-#'   ...
-#' }
-"ems_site_parameters"
-
-#' EMS sites.
-#'
-#' EMS lookup table for sites and their location.
-#'
-#' @format A data frame with 907694 rows and 22 variables:
-#' \describe{
-#'   \item{price}{price, in US dollars}
-#'   \item{carat}{weight of the diamond, in carats}
-#'   ...
-#' }
-"ems_sites"
--- data-raw/demo-data.R
+++ data-raw/demo-data.R
@@ -1,34 +1,13 @@
 library(rems)
-library(dplyr)
-library(ggplot2)
-library(checkr)
+library(usethis)
 
-demo_parameters <- c("Temperature", "pH", "Phosphorous Total", "Turbidity")
+demo_parameters <- c("Temperature", "pH", "Turbidity")
 demo_data_historic <- rems::read_historic_data(parameter = demo_parameters)
 demo_data_2yr <- rems::get_ems_data() %>%
   rems::filter_ems_data(parameter = demo_parameters)
 
 ems_demo_data <- rems::bind_ems_data(demo_data_historic, demo_data_2yr)
 
-demo_sites <- ems_demo_data$EMS_ID[1:1000]
-ems_demo_data %<>% filter(EMS_ID %in% demo_sites)
-
-ems_demo_parameters <- ems_demo_data %>%
-  group_by(EMS_ID, MONITORING_LOCATION, LATITUDE, LONGITUDE, LOCATION_TYPE, PARAMETER) %>%
-  summarise() %>%
-  ungroup()
-
-ems_demo_sites <- ems_demo_parameters %>%
-  group_by(EMS_ID, MONITORING_LOCATION, LATITUDE, LONGITUDE, LOCATION_TYPE) %>%
-  summarise() %>%
-  ungroup()
-
-check_key(ems_demo_sites, "EMS_ID")
-check_key(ems_demo_parameters, c("EMS_ID", "PARAMETER"))
-
-library(usethis)
-use_data(ems_demo_sites, overwrite = TRUE)
-use_data(ems_demo_parameters, overwrite = TRUE)
 use_data(ems_demo_data, overwrite = TRUE)
 
 
