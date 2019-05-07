79e28d0ffd97c1b2fc2644ea5c74ade5b71df89c
Seb Dalgarno <seb@poissonconsulting.ca>
2019-05-07 16:23:54 -0700

    download data if doesn't exist

--- R/checks.R
+++ R/checks.R
@@ -1,11 +1,14 @@
 check_2yr_data <- function(){
   message("checking for most recent 2 years of data...")
+  if(!rems:::._remsCache_$exists("2yr")){
+    rems::get_ems_data("2yr", ask = TRUE, check_only = FALSE)
+  }
   rems::get_ems_data("2yr", ask = FALSE, check_only = TRUE)
 }
 
 check_historic_data <- function(){
   message("checking for historic data...")
-  rems::download_historic_data(ask = FALSE)
+  rems::download_historic_data(ask = TRUE)
 }
 
 check_all_data <- function(){
