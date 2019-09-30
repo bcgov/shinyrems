e56f97b2f9c5f3c9d930a592fe346078397f2616
Seb Dalgarno <seb@poissonconsulting.ca>
2019-09-29 21:43:43 -0700

    remove old tests

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -13,27 +13,5 @@
 context("test-data")
 
 test_that("ems data functions work", {
-  data <- ems_demo_data
-  x <- site_to_emsid(data, c("SUN WAVE - MAIN OUTFALL",
-                             "KITIMAT RIVER STATION 40"))
-  expect_identical(x, c("0430022", "E103446"))
 
-  x <- parameter_to_site(data)
-  expect_is(x, "character")
-  expect_length(x, 115)
-
-  x <- parameter_to_location(data)
-  expect_is(x, "data.frame")
-  expect_identical(nrow(x), 115L)
-
-  x <- parameter_to_date(data)
-  expect_is(x, "Date")
-  expect_identical(x, as.Date(c("1966-08-18", "2019-03-06")))
-
-  x <- run_mode_data("demo")
-  expect_identical(data, x)
-
-  x <- demo_parameter()
-  expect_identical(x, c("Temperature", "Turbidity"))
-  expect_identical(run_mode_parameter("demo"), x)
 })
