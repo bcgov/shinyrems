9d7bcecd4f4691c7d104731c86cf953bec44bfe8
Seb Dalgarno <seb@poissonconsulting.ca>
2019-10-16 20:22:30 -0700

    make which argument explicit for clarity

--- R/functions_data.R
+++ R/functions_data.R
@@ -92,7 +92,7 @@ combine_data <- function(x, ...){
 }
 
 ems_data <- function(){
-  rems::get_ems_data(dont_update = TRUE, force = TRUE)
+  rems::get_ems_data(which = "2yr", dont_update = TRUE, force = TRUE)
 }
 
 run_mode_data <- function(run_mode, ...){
