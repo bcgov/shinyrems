31db6df36f0353660a22456e2d9f52dc9705c1ae
Seb Dalgarno <seb@poissonconsulting.ca>
2019-10-18 14:30:43 -0700

    move check to check script

--- R/utils.R
+++ R/utils.R
@@ -1,15 +1,5 @@
 is_try_error <- function(x) inherits(x, "try-error")
 
-check_template <- function(x, template){
-  x <- try(checkr::check_data(x = x,
-                     values = sapply(template, function(x) x$check, USE.NAMES = FALSE),
-                     nrow = c(1L,.Machine$integer.max),
-                     error = TRUE, x_name = "data"), silent = TRUE)
-  if(is_try_error(x))
-    return(gsub("Error : |\n", "", x[1]))
-  invisible(TRUE)
-}
-
 template_to_df <- function(template){
   x <- tibble::tibble(variable = c("example", "type", "description"))
   for(i in names(template)){
