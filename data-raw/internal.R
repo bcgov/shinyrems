949cc04bbf856982dc8da9b7ac0c2f7fd73155f3
Seb Dalgarno <seb@poissonconsulting.ca>
2019-11-07 11:10:05 -0800

    use single template and covert to raw with function

--- data-raw/internal.R
+++ data-raw/internal.R
@@ -22,24 +22,6 @@ lookup_2yr <- lookup(data_2yr)
 lookup_historic <- lookup(data_historic)
 lookup_demo <- lookup(shinyrems::ems_demo_data)
 
-template_tidy <- list(EMS = list(example = 123,
-                            type = "integer",
-                            description = "EMS ID",
-                            check = c(1, 124)),
-                 tidy = list(example = "yup",
-                             type = "character",
-                             description = "This is a test.",
-                             check = c("")))
-
-template_raw <- list(EMS = list(example = 123,
-                                 type = "integer",
-                                 description = "EMS ID",
-                                 check = c(1, 124)),
-                      raw = list(example = "yup",
-                                  type = "character",
-                                  description = "This is a test.",
-                                  check = c("")))
-
 watershed_groups <- st_read("~/Poisson/Data/spatial/fwa/gdb/FWA_BC.gdb", layer = "FWA_WATERSHED_GROUPS_POLY")
 watershed_groups <- ms_simplify(watershed_groups, keep = 0.01) %>%
   st_transform(4326)
@@ -48,6 +30,73 @@ watershed_groups$lng_center <- cent[,1]
 watershed_groups$lat_center <- cent[,2]
 
 watershed_groups <- watershed_groups %>% mutate_if(is.factor, as.character)
+
+template_tidy <- list(EMS_ID = list(example = "0124784",
+                                    type = "character",
+                                    description = "EMS ID of measurement.",
+                                    check = c("")),
+                      Station = list(example = "RUSSELL SLOUGH",
+                                     type = "character",
+                                     description = "Station of measurement.",
+                                     check = c("")),
+                      DateTime = list(example = "2018-02-21 13:30:00",
+                                      type = "character",
+                                      description = "Date and time of measurement in y-m-d h:m:s format.",
+                                      check = c(Sys.time())),
+                      Variable = list(example = "Aluminum Dissolved",
+                                      type = "character",
+                                      description = "Variable.",
+                                      check = c("")),
+                      Code = list(example = "AL-D",
+                                  type = "character",
+                                  description = "Parameter code.",
+                                  check = c("")),
+                      Value = list(example = 1.1,
+                                   type = "numeric",
+                                   description = "Value of measurement.",
+                                   check = c(0)),
+                      Units = list(example = "degC",
+                                   type = "character",
+                                   description = "Units of measurement.",
+                                   check = c("")),
+                      DetectionLimit = list(example = 0.01,
+                                            type = "number",
+                                            description = "Detection limit.",
+                                            check = c(1, NA)),
+                      ResultLetter = list(example = "<",
+                                          type = "character",
+                                          description = "Relationship to detection limit.",
+                                          check = c("<", ">", "M", NA)),
+                      SAMPLE_STATE = list(example = "Fresh Water",
+                                          type = "character",
+                                          description = "Sample state.",
+                                          check = c("", NA)),
+                      SAMPLE_CLASS = list(example = "Regular",
+                                          type = "character",
+                                          description = "Sample class.",
+                                          check = c("", NA)),
+                      SAMPLE_DESCRIPTOR = list(example = "General",
+                                          type = "character",
+                                          description = "Sample descriptor.",
+                                          check = c("", NA)),
+                      LOCATION_TYPE = list(example = "RIVER,STREAM OR CREEK",
+                                          type = "character",
+                                          description = "Location type.",
+                                          check = c("", NA)),
+                      UPPER_DEPTH = list(example = 0.1,
+                                           type = "numeric",
+                                           description = "Upper Depth.",
+                                           check = c(1, NA)),
+                      LOWER_DEPTH = list(example = 0.1,
+                                         type = "numeric",
+                                         description = "Lower Depth.",
+                                         check = c(1, NA)))
+
+raw_names <- c(EMS_ID = "EMS_ID", Station = "MONITORING_LOCATION",
+               DateTime = "COLLECTION_START", Variable = "PARAMETER",
+               Code = "PARAMETER_CODE", Value =  "RESULT",
+               Units = "UNIT", DetectionLimit = "METHOD_DETECTION_LIMIT",
+               ResultLetter = "RESULT_LETTER")
 
 datasets <- c("2yr", "historic", "all", "demo", "upload")
 
@@ -60,6 +109,6 @@ ems_reference_tables <- list("Collection Methods" = rems::ems_coll_methods,
 
 usethis::use_data(lookup_2yr, lookup_historic, lookup_demo,
                   template_raw, template_tidy, watershed_groups,
-                  datasets, ems_reference_tables,
+                  datasets, ems_reference_tables, raw_names,
                   internal = TRUE, overwrite = TRUE)
 
