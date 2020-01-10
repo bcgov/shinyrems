a24d3eec997690f097179525081ef37f5c59084d
Seb Dalgarno <seb@poissonconsulting.ca>
2020-01-09 16:54:49 -0800

    don't need lookup_2yr in internal data as cached

--- data-raw/internal.R
+++ data-raw/internal.R
@@ -6,7 +6,6 @@ library(poisspatial)
 library(sf)
 library(rmapshaper)
 
-data_2yr <- rems::get_ems_data(which = "2yr", dont_update = TRUE, force = TRUE)
 data_historic <- rems::read_historic_data(check_db = FALSE)
 
 lookup <- function(data){
@@ -23,7 +22,6 @@ lookup <- function(data){
   df
 }
 
-lookup_2yr <- lookup(data_2yr)
 lookup_historic2 <- lookup(data_historic)
 lookup_demo <- lookup(shinyrems::ems_demo_data)
 
@@ -75,14 +73,14 @@ raw_names <- c(EMS_ID = "EMS_ID", Station = "MONITORING_LOCATION",
                Units = "UNIT", DetectionLimit = "METHOD_DETECTION_LIMIT",
                ResultLetter = "RESULT_LETTER")
 
-datasets <- c("2yr", "historic", "all", "demo", "upload")
+datasets <- c("2yr", "4yr", "historic", "all", "demo", "upload")
 
 empty_df <- function(data){
   setNames(data.frame(matrix(ncol = length(names(data)),
                              nrow = 0)), names(data))
 }
 
-empty_raw <- empty_df(data_2yr)
+empty_raw <- empty_df(data_historic)
 
 tidy <- rems::get_ems_data(dont_update = TRUE) %>%
   rems::filter_ems_data(emsid = c("0121580"), parameter = c("pH")) %>%
@@ -106,7 +104,7 @@ ems_reference_tables <- list("Collection Methods" = rems::ems_coll_methods,
                              "Species" = rems::ems_species,
                              "Units" = rems::ems_units)
 
-usethis::use_data(lookup_2yr, lookup_historic, lookup_demo,
+usethis::use_data(lookup_historic, lookup_demo,
                   template_tidy, watershed_groups, empty_raw,
                   empty_tidy, empty_standard, empty_clean, empty_outlier,
                   datasets, ems_reference_tables, raw_names,
