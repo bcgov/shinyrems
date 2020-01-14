3735e67c707050ca5b13eb85537d88c4f4b9c0ea
Seb Dalgarno <seb@poissonconsulting.ca>
2020-01-13 21:38:38 -0800

    return all inputs

--- R/mod_data.R
+++ R/mod_data.R
@@ -352,9 +352,29 @@ mod_data_server <- function(input, output, session){
     }
   )
 
+  uiOutput(ns("ui_site")),
+  tags$label("Select Parameter(s)"),
+  radioButtons(ns("param_strict"), label = NULL,
+               choices = c("in ANY of selected sites",
+                           "in ALL of selected sites"),
+               selected = "in ANY of selected sites"),
+  uiOutput(ns("ui_parameter")),
+  uiOutput(ns("ui_date")),
+  uiOutput(ns("ui_get")),
+  br(),
+  uiOutput(ns("ui_sample_state")),
+  uiOutput(ns("ui_sample_class")),
+  uiOutput(ns("ui_mdl_action"))
+
   return(
     list(
-      data = filter_data
+      data = filter_data,
+      site = input$site,
+      parameter = input$parameter,
+      date = input$date,
+      sample_state = input$sample_state,
+      sample_class = input$sample_class,
+      mdl_action = input$md_action
     )
   )
 }
