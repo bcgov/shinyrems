6893622d33fab4c1aec74e0b89145b372794a830
Seb Dalgarno <seb@poissonconsulting.ca>
2020-03-16 18:28:09 -0700

    replace internal functions with rems::get_ems_lookup after PR

--- R/functions_lookup.R
+++ R/functions_lookup.R
@@ -1,14 +1,14 @@
 get_lookup <- function(dataset){
   lookup <- NULL
   if(dataset %in% c("2yr", "4yr"))
-    lookup <- get_ems_lookup(dataset)
+    lookup <- rems::get_ems_lookup(dataset)
 
   if(dataset == "historic")
     lookup <- lookup_historic
 
   if(dataset == "all"){
     lookup1 <- lookup_historic
-    lookup2 <- get_ems_lookup("2yr")
+    lookup2 <- rems::get_ems_lookup("2yr")
     lookup <- rbind(lookup1, lookup2)
   }
 
