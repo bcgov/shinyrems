ebbb7805b388818f6672439833b89419ad34105b
Seb Dalgarno <seb@poissonconsulting.ca>
2020-03-22 20:14:02 -0700

    update internal data

--- data-raw/internal.R
+++ data-raw/internal.R
@@ -105,45 +105,45 @@ ems_reference_tables <- list("Collection Methods" = rems::ems_coll_methods,
                              "Units" = rems::ems_units)
 
 ## code to prepare `limits` dataset goes here
-limits2 <- bcdata::bcdc_get_data(record = "85d3990a-ec0a-4436-8ebd-150de3ba0747")
-limits_new <- dplyr::mutate(limits2,
-                            Condition = dplyr::if_else(Condition == "",
-                                                       NA_character_, Condition)) %>%
-  dplyr::filter(Use == "Aquatic Life - Freshwater",
-                Direction == "Upper Limit",
-                Media == "Water") %>%
-  dplyr::mutate(Use = "Freshwater Life") %>%
-  dplyr::mutate(Term = dplyr::if_else(Days == 30, "Long", "Short")) %>%
-  ### cases with multiple EMS_CODE
-  dplyr::group_by(Variable) %>%
-  dplyr::mutate(EMS_Code2 = dplyr::first(EMS_Code)) %>%
-  dplyr::ungroup() %>%
-  dplyr::filter(EMS_Code2 == EMS_Code) %>%
-  ### remove cases with ConditionNotes
-  dplyr::filter(is.na(ConditionNotes)) %>%
-  dplyr::mutate(Variable = paste(Variable, Component),
-                UpperLimit = Limit) %>%
-  dplyr::select(Variable, Use, Term,
-                Condition, UpperLimit, Units,
-                Statistic, EMS_Code)
+# limits2 <- bcdata::bcdc_get_data(record = "85d3990a-ec0a-4436-8ebd-150de3ba0747")
+# limits_new <- dplyr::mutate(limits2,
+#                             Condition = dplyr::if_else(Condition == "",
+#                                                        NA_character_, Condition)) %>%
+#   dplyr::filter(Use == "Aquatic Life - Freshwater",
+#                 Direction == "Upper Limit",
+#                 Media == "Water") %>%
+#   dplyr::mutate(Use = "Freshwater Life") %>%
+#   dplyr::mutate(Term = dplyr::if_else(Days == 30, "Long", "Short")) %>%
+#   ### cases with multiple EMS_CODE
+#   dplyr::group_by(Variable) %>%
+#   dplyr::mutate(EMS_Code2 = dplyr::first(EMS_Code)) %>%
+#   dplyr::ungroup() %>%
+#   dplyr::filter(EMS_Code2 == EMS_Code) %>%
+#   ### remove cases with ConditionNotes
+#   dplyr::filter(is.na(ConditionNotes)) %>%
+#   dplyr::mutate(Variable = paste(Variable, Component),
+#                 UpperLimit = Limit) %>%
+#   dplyr::select(Variable, Use, Term,
+#                 Condition, UpperLimit, Units,
+#                 Statistic, EMS_Code)
+#
+# ### remove mercury duplicate if hasnt been resolved yet on bcdata
+# mercury_index <- limits_new$Variable == "Mercury Total" & limits_new$UpperLimit == 0.02
+#
+# if(is.na(limits_new$Condition[mercury_index])){
+#   limits_new <- limits_new[!mercury_index,]
+# }
+#
+# ### ensure that no duplicates
+# expect_true(all(limits_new %>%
+#                   dplyr::group_by(Variable, Use, Term, Condition) %>%
+#                   dplyr::mutate(n = dplyr::n()) %>%
+#                   dplyr::ungroup() %>%
+#                   dplyr::pull(n)))
+#
+# limits <- limits_new
 
-### remove mercury duplicate if hasnt been resolved yet on bcdata
-mercury_index <- limits_new$Variable == "Mercury Total" & limits_new$UpperLimit == 0.02
-
-if(is.na(limits_new$Condition[mercury_index])){
-  limits_new <- limits_new[!mercury_index,]
-}
-
-### ensure that no duplicates
-expect_true(all(limits_new %>%
-                  dplyr::group_by(Variable, Use, Term, Condition) %>%
-                  dplyr::mutate(n = dplyr::n()) %>%
-                  dplyr::ungroup() %>%
-                  dplyr::pull(n)))
-
-limits <- limits_new
-
-usethis::use_data(limits, lookup_historic, lookup_demo,
+usethis::use_data(lookup_historic, lookup_demo,
                   template_tidy, watershed_groups, empty_raw,
                   empty_tidy, empty_standard, empty_clean, empty_outlier,
                   datasets, ems_reference_tables, raw_names,
