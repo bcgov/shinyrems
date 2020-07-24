8ea137994e56fce19d122ee0a5d3ad35bb745a77
Seb Dalgarno <seb@poissonconsulting.ca>
2020-07-23 17:20:53 -0700

    update upload tests

--- tests/testthat/test-upload.R
+++ tests/testthat/test-upload.R
@@ -15,27 +15,19 @@ test_that("data upload works", {
                         dataset = "upload",
                         cols = character(0)
   )
-  tidy_data2 <- wqbc::tidy_ems_data(data, mdl_action = "zero", cols = character(0))
-  expect_identical(tidy_data, tidy_data2)
-
-  data2 <- data %>%
-    dplyr::select(Station, Variable, DateTime, Value, Units)
+  expect_identical(nrow(tidy_data), 2L)
 
   stand_data <- ems_standardize(data, TRUE)
-  stand_data2 <- ems_standardize(data2, TRUE)
+  expect_identical(nrow(stand_data), 2L)
 
   agg_data <- ems_aggregate(stand_data,
                             by = "Station", remove_blanks = FALSE,
                             max_cv = Inf, FUN = max
   )
-  agg_data2 <- ems_aggregate(stand_data2,
-                            by = "Station", remove_blanks = FALSE,
-                            max_cv = Inf, FUN = max
-  )
-  expect_identical(nrow(agg_data), 30L)
+  expect_identical(nrow(agg_data), 2L)
 
-  out_data <- ems_outlier(stand_data2, by = "Station", max_cv = Inf, sds = 1, FUN = mean)
-  expect_identical(nrow(out_data), 30L)
-  data
+  out_data <- ems_outlier(stand_data, by = "Station", max_cv = Inf, sds = 1, FUN = mean)
+  expect_identical(nrow(stand_data), 2L)
+
 
 })
