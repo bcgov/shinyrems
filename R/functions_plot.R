f26327051f9c74e1e209e6239fec022fbf132c0c
Seb Dalgarno <seb@poissonconsulting.ca>
2020-07-31 14:38:43 -0700

    guideline line width

--- R/functions_plot.R
+++ R/functions_plot.R
@@ -48,20 +48,24 @@ ems_plot_base <- function(data, facet){
 
 ems_plot_add_guideline <- function(gp, guideline){
   if (!is.data.frame(guideline)) {
-    gp <- gp + ggplot2::geom_hline(yintercept = guideline, linetype = "dotted")
+    gp <- gp + ggplot2::geom_hline(yintercept = guideline,
+                                   linetype = "dotted",
+                                   size = 1)
   }
 
   if (is.data.frame(guideline)) {
     if (nrow(guideline) == 1) {
       gp <- gp + ggplot2::geom_hline(
         yintercept = guideline$UpperLimit,
-        linetype = "dotted"
+        linetype = "dotted",
+        size = 1
       )
     } else {
       gp <- gp + ggplot2::geom_line(
         data = guideline,
         ggplot2::aes_string(x = "Date", y = "UpperLimit"),
-        linetype = "dotted"
+        linetype = "dotted",
+        size = 1
       )
     }
   }
